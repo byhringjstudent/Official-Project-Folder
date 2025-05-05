b9dc866dc8b813767c97bc3118f9116935bca10e
MichaelM <mikeymaher07@live.com>
2025-05-05 16:23:55 -0500

    cleaned up some files, added some comments, and updated the account db functions to be uniform.

--- Backend/utils.py
+++ Backend/utils.py
@@ -1,5 +1,7 @@
 import uuid 
 
+#Purpose: This file contains utility functions for the application.
+# It include a function for generating unique IDs
 def create_unique_id():
     random_key = uuid.uuid4()
     genid = str(random_key)
